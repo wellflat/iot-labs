81a2d924ba1fe0d79fd01f499f1a2e5d83444379
wellflat <wellflat@gmail.com>
2016-08-30 00:43:25 +0900

    added post data samples

---
+++ m2x/m2xlib_sample.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import yaml
+from pprint import pprint
+import Adafruit_BMP.BMP085 as BMP
+from m2x.client import M2XClient
+
+## using M2X client library sample
+if __name__ == '__main__':
+    cur_dir = os.path.abspath(os.path.dirname(__file__))
+    os.chdir(cur_dir)
+    
+    with open('m2x.yml', 'r') as f:
+        conf = yaml.load(f)
+        api_key = conf['api_key']
+        device_id = conf['device_id']
+        stream_ids = conf['stream']
+        
+    client = M2XClient(key=api_key)
+    device = client.device(device_id)
+    #pprint(device.data)
+    stream_temp = device.stream(stream_ids[0])
+    stream_pres = device.stream(stream_ids[1])
+    sensor = BMP.BMP085()
+    temp = sensor.read_temperature()
+    pres = sensor.read_pressure()
+    print('Temperature: %lf' % temp)
+    print('Temperature: %lf' % pres)
+    stream_temp.add_value(temp)
+    print('Status: %d' % client.last_response.status)
+    stream_pres.add_value(pres)
+    print('Status: %d' % client.last_response.status)
+    print(client.last_response.raw)
---
+++ m2x/pycurl_sample.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import datetime
+import json
+import yaml
+import pycurl
+from pprint import pprint
+import Adafruit_BMP.BMP085 as BMP
+
+## using pycurl sample
+if __name__ == '__main__':
+    cur_dir = os.path.abspath(os.path.dirname(__file__))
+    os.chdir(cur_dir)
+    sensor = BMP.BMP085() ## you can read BMP180 data
+    temp = sensor.read_temperature()
+
+    # BMP180 read data tests
+    print('Temp = {0:0.2f} *C'.format(sensor.read_temperature()))
+    print('Temp = {0:0.2f} *C'.format(sensor.read_raw_temp()))
+    print('Pressure = {0:0.2f} Pa'.format(sensor.read_pressure()))
+    print('Altitude = {0:0.2f} m'.format(sensor.read_altitude()))
+    print('Sealevel Pressure = {0:0.2f} Pa'.format(sensor.read_sealevel_pressure()))
+
+    with open('m2x.yml', 'r') as f:
+        conf = yaml.load(f)
+        api_key = conf['api_key']
+        url = conf['base_url'] + '/' + conf['stream'][0] + '/value'
+
+    timestamp = datetime.datetime.now().isoformat()
+    data = {'value': temp}  ## required 'value', 'timestamp' keys
+    field_data = json.dumps(data)
+    
+    ch = pycurl.Curl()
+    ch.setopt(pycurl.CUSTOMREQUEST, 'PUT')
+    ch.setopt(pycurl.URL, url)
+    ch.setopt(pycurl.VERBOSE, True)
+    headers = ('X-M2X-KEY: ' + api_key, "Content-Type: application/json")
+    ch.setopt(pycurl.HTTPHEADER, headers)
+    print(field_data)
+    ch.setopt(pycurl.POSTFIELDS, field_data)
+    ch.perform()
+    print('M2X Status: %d' % ch.getinfo(pycurl.RESPONSE_CODE))
