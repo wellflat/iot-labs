125d662d5e58521ca6403eb27369bcf68683091e
wellflat <wellflat@gmail.com>
2016-09-04 00:16:49 +0900

    added AWS IoT publish message sample

---
+++ aws/publish.js
@@ -0,0 +1,19 @@
+
+var awsIot = require('aws-iot-device-sdk');
+var fs = require('fs');
+
+var conf = JSON.parse(fs.readFileSync('cert.json', 'utf8'));
+
+var device = awsIot.device({
+  region: conf.region,
+  clientId: conf.clientId,
+  privateKey: conf.privateKey,
+  clientCert: conf.clientCert,
+  caCert: conf.caCert
+});
+
+device.on('connect', function() {
+  console.log('connect');
+  device.publish(conf.topic, 'test message');
+  console.log('published');
+});
